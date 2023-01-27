cae39c3eb346f605bf2726dba562be85c8975ef7
tk-yoshimura <yoshimura8192@gmail.com>
2023-01-27 16:01:47 +0900

    Update Matrix_GaussianEliminate.cs

--- Algebra/Matrix_GaussianEliminate.cs
+++ Algebra/Matrix_GaussianEliminate.cs
@@ -83,7 +83,7 @@ namespace Algebra {
             return v;
         }
 
-        /// <summary>連立方程式</summary>
+        /// <summary>連立方程式の解</summary>
         public static Vector Solve(Matrix m, Vector v) {
             if (!IsSquare(m) || m.Size != v.Dim) {
                 throw new ArgumentException("invalid size", $"{nameof(m)}, {nameof(v)}");
