38560c6186add5f6a43d6fcebe26426f1570094e
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-05 22:46:53 +0900

    add: cholesky

---
+++ Algebra/Matrix/Matrix_Cholesky.cs
@@ -0,0 +1,50 @@
+﻿using DoubleDouble;
+using System;
+
+namespace Algebra {
+    /// <summary>行列クラス</summary>
+    public partial class Matrix {
+        /// <summary>コレスキー分解</summary>
+        public static Matrix Cholesky(Matrix m, bool enable_check_symmetric = true) {
+            if (!IsSquare(m)) {
+                throw new ArgumentException("not square matrix", nameof(m));
+            }
+
+            int n = m.Size;
+            
+            if ((enable_check_symmetric && !IsSymmetric(m)) || !IsFinite(m)) {
+                return Invalid(n);
+            }
+
+            if (IsZero(m)) {
+                return Zero(n);
+            }
+
+            int exponent = (m.MaxExponent / 2) * 2;
+
+            Matrix u = ScaleB(m, -exponent);
+
+            ddouble[,] v = new ddouble[n, n];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    ddouble v_ij = u[i, j];
+                    for (int k = 0; k < j; k++) {
+                        v_ij -= v[i, k] * v[j, k];
+                    }
+                    v[i, j] = v_ij / v[j, j]; 
+                }
+
+                ddouble v_ii = u[i, i];
+                for (int k = 0; k < i; k++) {
+                    v_ii -= v[i, k] * v[i, k];
+                }
+                v[i, i] = ddouble.Sqrt(v_ii);
+            }
+
+            Matrix l = ScaleB(new Matrix(v, cloning: false), exponent / 2);
+
+            return l;
+        }
+    }
+}
---
+++ AlgebraTests/Matrix/MatrixTests_Cholesky.cs
@@ -0,0 +1,19 @@
+﻿using Algebra;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AlgebraTests {
+    public partial class MatrixTests {
+        [TestMethod()]
+        public void CholeskyTest() {
+            foreach (Matrix matrix in MatrixTestCases.PositiveMatrixs) {
+                Console.WriteLine($"test: {matrix}");
+
+                Matrix l = Matrix.Cholesky(matrix);
+                Matrix v = l * l.T;
+
+                Assert.IsTrue((matrix - v).Norm < 1e-28);
+            }
+        }
+    }
+}
